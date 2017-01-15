2e83870f13b70cf0708dcd33cd1b33c2aab538be
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-16 00:52:20 +0200

    Update production deploy script

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-echo 'Deploying to production environment!'
-docker service create --replicas 1 --constraint 'node.labels.deploy-env == prod' --publish 9000:80 --name sample-with-tests my-registry:8082/sample-with-tests
+imgName='sample-with-tests'
+echo 'Deploying to dev environment'
+if docker service inspect $imgName ; then
+  echo 'Service already exists, updating'
+  docker service update --image my-registry:8082/$imgName:0.1.0 $imgName
+else
+  echo 'Service does not already exist, creating'
+  docker service create --replicas 1 --constraint 'node.labels.deploy-env == prod' --publish 9001:80 --name $imgName my-registry:8082/$imgName:0.1.0
+fi
