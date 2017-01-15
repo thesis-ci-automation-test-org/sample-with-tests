0f772fd83870925cfa119c050f471538aa0488ab
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-16 00:29:30 +0200

    Add actual Docker Swarm deploy scripts

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo 'Deploying to dev environment'
-echo 'DONE'
+docker service create --replicas 1 --constraint 'node.labels.deploy-env == dev' --publish 9000:80 --name sample-with-tests my-registry:8082/sample-with-tests
--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo 'Deploying to production environment!'
-echo 'DONE'
+docker service create --replicas 1 --constraint 'node.labels.deploy-env == prod' --publish 9000:80 --name sample-with-tests my-registry:8082/sample-with-tests
