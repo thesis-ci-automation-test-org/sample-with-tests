39916fb899c830a4f84d5350769f9f3383263621
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-16 00:44:54 +0200

    Try fixing Docker Swarm deploy

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+imgName='sample-with-tests'
 echo 'Deploying to dev environment'
-docker service create --replicas 1 --constraint 'node.labels.deploy-env == dev' --publish 9000:80 --name sample-with-tests my-registry:8082/sample-with-tests
+if docker service inspect $imgName ; then
+  echo 'Service already exists, updating'
+  docker service update --image my-registry:8082/$imgName $imgName
+else
+  echo 'Service does not already exist, creating'
+  docker service create --replicas 1 --constraint 'node.labels.deploy-env == dev' --publish 9000:80 --name $imgName my-registry:8082/$imgName
+fi
