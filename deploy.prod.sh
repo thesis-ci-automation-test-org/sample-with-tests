a44f86af6a8d4df72a75be23a03a550d143c24ac
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-16 14:34:16 +0200

    Fix prod deploy

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -4,7 +4,7 @@ imgName='sample-with-tests'
 echo 'Deploying to dev environment'
 if docker service inspect $imgName ; then
   echo 'Service already exists, updating'
-  docker service update --container-label-add last_deployed=$(date -u +%Y-%m-%dT%H:%M:%S) $imgName-prod
+  docker service update --image my-registry:8082/$imgName:0.1.1 --container-label-add last_deployed=$(date -u +%Y-%m-%dT%H:%M:%S) $imgName-prod
 else
   echo 'Service does not already exist, creating'
   docker service create --replicas 1 --constraint 'node.labels.deploy-env == prod' --publish 9001:80 --name $imgName-prod my-registry:8082/$imgName:0.1.1
