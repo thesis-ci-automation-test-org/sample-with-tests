0199766919e5d48c11233142e3d7548db413c9ad
Mikko Piuhola <mikko.piuhola@gmail.com>
2017-01-16 14:17:27 +0200

    Force re-deploy in Docker Swarm

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -4,7 +4,7 @@ imgName='sample-with-tests'
 echo 'Deploying to dev environment'
 if docker service inspect $imgName ; then
   echo 'Service already exists, updating'
-  docker service update --image my-registry:8082/$imgName $imgName
+  docker service update --container-label-add last_deployed=$(date -u +%Y-%m-%dT%H:%M:%S) $imgName
 else
   echo 'Service does not already exist, creating'
   docker service create --replicas 1 --constraint 'node.labels.deploy-env == dev' --publish 9000:80 --name $imgName my-registry:8082/$imgName
